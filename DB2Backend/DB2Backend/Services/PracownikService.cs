b2a51fec626f18bd71281b90838cc3e6494ce7dc
YaridTheBoi <kacper.chrostowski@gmail.com>
2023-01-17 16:15:22 +0100

    chyba bedzie ddziaac

--- DB2Backend/DB2Backend/Services/PracownikService.cs
+++ DB2Backend/DB2Backend/Services/PracownikService.cs
@@ -12,9 +12,12 @@ namespace DB2Backend.Services
             var doesExist = context.Obywatels.FirstOrDefault(m => m.Pesel == input.PESEL);
             if(doesExist == null)
             {
+                Console.Write((input.PESEL).Length);
+                Console.Write($"wstawNowegoObywatelaZAdresem '{input.Imie}','{input.Nazwisko}','{input.PESEL}',{DateTime.Parse(input.Data)},{input.Plec},{input.Wojewodztwo},{input.Konto}, {input.Dokument},'{input.Kraj}',' {input.Miasto}','{input.Ulica}',{input.Nr_domu},{input.Nr_mieszkania};");
                // var res = context.wstawNowegoObywatelaZAdresem.FromSql($"wstawNowegoObywatelaZAdresem {input.Imie}, {input.Nazwisko}, {input.PESEL}, {input.Data}, {input.Plec}, {input.Wojewodztwo}, {input.Konto}, {input.Dokument}, {input.Kraj}, {input.Miasto}, {input.Ulica}, {input.Nr_domu}, {input.Nr_mieszkania}");
-                //var res=context.Database.ExecuteSql($"wstawNowegoObywatelaZAdresem '{input.Imie}','{input.Nazwisko}','{input.PESEL}',{input.Data},{input.Plec},{input.Wojewodztwo},{input.Konto}, {input.Dokument},'{input.Kraj}',' {input.Miasto}','{input.Ulica}',{input.Nr_domu},{input.Nr_mieszkania};");
-                //Console.Write( res );
+                var res=context.Database.ExecuteSql($"execute wstawNowegoObywatelaZAdresem '{input.Imie}','{input.Nazwisko}','{input.PESEL}','{DateTime.Parse(input.Data)}',{input.Plec},{input.Wojewodztwo},{input.Konto}, {input.Dokument},'{input.Kraj}',' {input.Miasto}','{input.Ulica}',{input.Nr_domu},{input.Nr_mieszkania};");  //datetime sie pierdoli trzeba poprawic
+
+                Console.Write( res );
                 return 1;
             }
 
